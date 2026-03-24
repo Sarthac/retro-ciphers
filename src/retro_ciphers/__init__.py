dd7904c3b14e8c3fbe81bf7c3bad6c65e6d4edd7
sarthac <hi99@tutanota.com>
2026-03-24 08:06:24 +0530

    add direct access to cipher classes, write docstring

--- src/retro_ciphers/__init__.py
+++ src/retro_ciphers/__init__.py
@@ -1,3 +1,55 @@
-# from .base import *
-from .mono import *
-from .poly import *
+"""Classical implementation of Substitution ciphers.
+
+This package provides implementation of Monoalphabetic substitution, Polyalphabetic substitution cipher.
+Algorithms that Monoalphabetic substitution includes Atbash, Baconian, Caesar, MixedAlphabet, PolybiusSquare,
+Rot13, Shift and SimpleSubstitution; Polyalphabetic substitution includes Alberti, Autokey, Beaufort, Trithemius,
+and Vigenere.
+
+Modules:
+    mono: Monoalphabetic substitution that map each letter to its corresponding fixed letter.
+    poly: Polyalphabetic substitution that uses multiple shift rules and then map each letter to its corresponding
+    fixed letter.
+"""
+
+
+# compatible with older code i.e. retro_ciphers.mono and retro_ciphers.poly
+from . import mono, poly
+# direct access to classes
+from .mono import (
+    Atbash,
+    Baconian,
+    Caesar,
+    MixedAlphabet,
+    PolybiusSquare,
+    Rot13,
+    Shift,
+    SimpleSubstitution,
+)
+from .poly import (
+    Alberti,
+    Autokey,
+    Beaufort,
+    Trithemius,
+    Vigenere,
+)
+
+__all__ : list[str] = [
+    "mono",
+    "poly",
+    # Monoalphabetic
+    "Atbash",
+    "Shift",
+    "Caesar",
+    "Rot13",
+    "MixedAlphabet",
+    "SimpleSubstitution",
+    "Baconian",
+    "PolybiusSquare",
+
+    # Polyalphabetic
+    "Alberti",
+    "Trithemius",
+    "Vigenere",
+    "Beaufort",
+    "Autokey",
+]
